7ac240546f999d2b110a2f087d5d53a70438ecdd
Gabriel Avila <86084641+SalMolhado@users.noreply.github.com>
2023-07-03 19:37:59 -0300

    Update interseção de doenças associadas a sintomas e fatores de risco.sql

--- Data/Querying/interseção de doenças associadas a sintomas e fatores de risco.sql
+++ Data/Querying/interseção de doenças associadas a sintomas e fatores de risco.sql
@@ -4,17 +4,26 @@
 -- Tabelas envolvidas: Doenças, DoençaSintoma, DoencaFatorDeRisco.
 
 
-SELECT D1.nome_doenca
-FROM Doencas D1
-WHERE D1.id_doenca IN 
+SELECT D.nome_doenca
+FROM Doencas D
+WHERE D.id_doenca IN 
 (
     SELECT DS.id_doenca 
     FROM DoencaSintoma DS
     WHERE DS.id_sintoma IN %s
 ) 
-AND D1.id_doenca IN 
+AND D.id_doenca IN 
 (
     SELECT DFR.id_doenca 
     FROM DoencaFatorDeRisco DFR
     WHERE DFR.id_fatorderisco IN %s
 );
+
+-- 3/7 ~ também poderia ser:
+--SELECT D.nome_doenca
+--FROM Doencas D
+--JOIN DoencaSintoma DS ON D.id_doenca = DS.id_doenca
+--JOIN DoencaFatorDeRisco DFR ON D.id_doenca = DFR.id_doenca
+--WHERE DS.id_sintoma IN %s
+--AND DFR.id_fatorderisco IN %s
+--GROUP BY D.nome_doenca;
